536d914f2b3bce5b60b3f2912950e2e4206e3eca
hujeong <hujeong@suresofttech.com>
2023-07-24 17:37:18 +0900

    fix

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   testEnvironment: "node",
+  coverageThreshold: {
+      global: {
+          lines: 90,
+          statements: 100,
+          functions: 100,
+          branches: 50
+      },
+  },
 };
