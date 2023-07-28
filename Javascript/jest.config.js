966956a23450248916db419729050fe74f5d7360
hujeong <hujeong@suresofttech.com>
2023-07-28 09:20:06 +0900

    threshold 삭제

--- Javascript/jest.config.js
+++ Javascript/jest.config.js
@@ -1,11 +1,3 @@
 module.exports = {
   testEnvironment: "node",
-  coverageThreshold: {
-      global: {
-          lines: 90,
-          statements: 100,
-          functions: 100,
-          branches: 50
-      },
-  },
 };
